68a786ac6334833f34d81fa39d11da193f914d5b
matias tang <matiastang@163.com>
2020-07-22 15:32:30 +0800

    添加IQKeyboardManager三方库

--- src/MT-RxSwiftLearning/MT-RxSwiftLearning/AppDelegate.swift
+++ src/MT-RxSwiftLearning/MT-RxSwiftLearning/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import IQKeyboardManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 启用IQKeyboardManager
+        IQKeyboardManager.shared().isEnabled = true
+        
         return true
     }
 
